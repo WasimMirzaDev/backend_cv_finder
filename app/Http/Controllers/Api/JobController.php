c44557e41560750db6ed3f0aa8ba46f5843d6f2a
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-08-29 12:17:55 +0500

    new backend changes

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -21,10 +21,10 @@ class JobController extends Controller
                 'x-rapidapi-key' => 'a007b488d3msh5fe56b4d9e822b1p1fb2bcjsnf9de8d4ed9ee', // TODO: Move to .env
             ])->get('https://jsearch.p.rapidapi.com/search', [
                 'query' => $query,
-                'page' => $request->input('page', 1),
+                'page' => $request->input('page', 2),
                 'num_pages' => $request->input('num_pages', 1),
                 'country' => $request->input('gl', 'us'), // Mapped from 'gl'
-                'date_posted' => $request->input('date_posted', 'all'),
+                'date_posted' => $request->input('date_posted', '3days'),
             ]);
 
 
