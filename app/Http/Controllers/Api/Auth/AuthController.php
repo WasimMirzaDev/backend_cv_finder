f9a010edb1dfa8d741ee2707e8c8cbfc831c77f5
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-09-18 16:39:24 +0500

    new backednc changes

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\PendingUser;
-use App\Models\GettingGettingStartedStep;
+use App\Models\GettingStartedStep;
 // use App\Services\TwilioService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
