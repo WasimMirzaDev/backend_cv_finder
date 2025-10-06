1de6282d96670183e134a90e2d8c3a87873b0de7
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-10-06 18:03:16 +0500

    new changes

--- app/Http/Controllers/Api/Auth/PasswordResetController.php
+++ app/Http/Controllers/Api/Auth/PasswordResetController.php
@@ -35,7 +35,7 @@ class PasswordResetController extends Controller
             ]);
     
             // Send email with reset link
-            $resetUrl = config('app.frontend_url') . "/reset-password?token=" . $token . "&email=" . urlencode($email);
+            $resetUrl = "https://midnightblue-yak-450228.hostingersite.com/update-password?token=" . $token . "&email=" . urlencode($email);
             
             try {
                 Mail::to($email)->send(new PasswordResetMail($resetUrl));
