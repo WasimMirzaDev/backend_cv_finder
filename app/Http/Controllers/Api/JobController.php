e4bddc23eb5cc2bb78df717ea1a9793be145b32c
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-10-17 17:18:19 +0500

    new pages

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -26,7 +26,7 @@ class JobController extends Controller
             ])->get('https://jsearch.p.rapidapi.com/search', [
                 'query' => $query,
                 'page' => $request->input('page', 1),
-                'num_pages' => $request->input('num_pages', 10),
+                'num_pages' => $request->input('num_pages', 3),
                 'country' => $request->input('gl', 'us'), // Mapped from 'gl'
                 'date_posted' => $request->input('date_posted', 'week'),
             ]);
